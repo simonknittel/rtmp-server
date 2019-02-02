f0133b9aaff05224c6a9cc620e8f017fe65b91a1
Ind3xOnGitHub <me@ind3x.site>
2019-02-02 20:41:17 +0100

    Start to convert from anime.js to CSS animations

--- webroot/scripts.js
+++ webroot/scripts.js
@@ -1,3 +1,4 @@
+const pageWrapper = document.querySelector('.page-wrapper')
 const form = document.querySelector('form')
 const keyInput = form.querySelector('input')
 const errorMessage = form.querySelector('.error')
@@ -7,139 +8,13 @@ const ctx = canvas.getContext('2d')
 let initial = true
 let keyByParamter = false
 
-const t1 = anime.timeline({
-  autoplay: false,
-  easing: 'easeInOutBack'
-})
-
-const t2 = anime.timeline({
-  autoplay: false,
-  easing: 'easeInOutBack',
-})
-
-t1
-  .add({
-    targets: 'button',
-    opacity: [{
-      duration: 500,
-      value: 0
-    }]
-  }, 0)
-
-  .add({
-    targets: 'input',
-    color: [{
-      duration: 500,
-      value: '#111'
-    }],
-    paddingLeft: [{
-      delay: 300,
-      duration: 1,
-      value: 0
-    }],
-    paddingRight: [{
-      delay: 300,
-      duration: 1,
-      value: 0
-    }]
-  }, 0)
-
-  .add({
-    targets: 'form',
-    maxWidth: [{
-      duration: 750,
-      value: '65px'
-    }]
-  }, 0)
-
-  .add({
-    targets: '.loady',
-    opacity: [{
-      delay: 750,
-      duration: 250,
-      value: 1
-    }]
-  }, 0)
-
-t2
-  .add({
-    targets: 'button',
-    opacity: [{
-      delay: 2000,
-      duration: 1,
-      value: 1
-    }]
-  }, 0)
-
-  .add({
-    targets: 'input',
-    color: [{
-      delay: 2000,
-      duration: 1,
-      value: '#fff'
-    }],
-    paddingLeft: [{
-      delay: 1700,
-      duration: 1,
-      value: '1rem'
-    }],
-    paddingRight: [{
-      delay: 1700,
-      duration: 1,
-      value: '8rem'
-    }]
-  }, 0)
-
-  .add({
-    targets: 'form',
-    maxWidth: [{
-      delay: 2000,
-      duration: 1,
-      value: '480px'
-    }],
-    translateY: [{
-      delay: 1500,
-      duration: 500,
-      value: -200
-    }, {
-      delay: 500,
-      duration: 1,
-      value: 0
-    }],
-    opacity: [{
-      delay: 750,
-      duration: 500,
-      value: 1
-    }]
-  }, 0)
-
-  .add({
-    targets: '.loady',
-    opacity: [{
-      delay: 1250,
-      duration: 1,
-      value: 0
-    }]
-  }, 0)
-
-  .add({
-    targets: '.video-wrapper',
-    height: {
-      delay: 2000,
-      duration: 1000,
-      value: video.scrollHeight,
-      easing: 'easeInOutExpo'
-    }
-  }, 0)
-
 function playT1() {
-  t1.direction = 'normal'
-  t1.play()
+  pageWrapper.classList.add('t1')
 }
 
 function reverseT1(error) {
-  t1.direction = 'reverse'
-  t1.play()
+  // TODO: Reverse play CSS animation
+  pageWrapper.classList.remove('t1')
 
   if (!error) return
   t1.finished.then(() => {
@@ -153,23 +28,23 @@ function showVideo() {
   if (!initial) return
   initial = true
 
-  console.trace('t2.play()')
-  t2.play()
-  t2.finished.then(() => {
-    if (keyByParamter) errorMessage.Text = 'Your video is muted'
-
-    document.querySelector('.video-wrapper').style.overflow = 'visible'
-    document.querySelector('.video-wrapper').style.height = 'auto'
-
-    window.setInterval(() => {
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
-    }, 50)
-  })
+  pageWrapper.classList.add('t2')
+  // t2.finished.then(() => {
+  //   if (keyByParamter) errorMessage.Text = 'Your video is muted'
+  //
+  //   document.querySelector('.video-wrapper').style.overflow = 'visible'
+  //   document.querySelector('.video-wrapper').style.height = 'auto'
+  //
+  //   window.setInterval(() => {
+  //     ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+  //   }, 50)
+  // })
 }
 
 function onSuccess() {
-  if (t1.completed) showVideo() // t1 finished before manifest loaded
-  else t1.finished.then(showVideo) // t1 finishes after manifest loads
+  // if (t1.completed) showVideo() // t1 finished before manifest loaded
+  // else t1.finished.then(showVideo) // t1 finishes after manifest loads
+  showVideo()
 
   video.play()
   video.focus()
@@ -181,8 +56,9 @@ function onError(_, error) {
     return
   }
 
-  if (t1.completed) reverseT1(true)
-  else t1.finished.then(reverseT1.bind(null, true)) // TODO: Show error message
+  // if (t1.completed) reverseT1(true)
+  // else t1.finished.then(reverseT1.bind(null, true)) // TODO: Show error message
+  reverseT1()
 
   keyInput.focus()
   console.error(error)
