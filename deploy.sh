2358e248020010e026f05475634ac1f18cf92946
Ind3xOnGitHub <me@ind3x.site>
2019-02-05 20:02:29 +0100

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -r Dockerfile nginx.conf webroot $USER@$HOST:$LOCATION
+rsync -r Dockerfile nginx.conf webroot $D_USER@$D_HOST:$D_LOCATION
 
-ssh $USER@$HOST "docker rm -f streaming-server && docker build -t streaming-server $LOCATION"
-ssh $USER@$HOST "docker run -d -p 1935:1935 -p 80:80 --name streaming-server --restart always streaming-server"
+ssh $D_USER@$D_HOST "docker rm -f streaming-server && docker build -t streaming-server $D_LOCATION"
+ssh $D_USER@$D_HOST "docker run -d -p 1935:1935 -p 80:80 --name streaming-server --restart always streaming-server"
