0a43191376eee3adb554cd793ffa2d12558ab60b
Ind3xOnGitHub <me@ind3x.site>
2019-02-08 20:02:03 +0100

    Remove Travis CI config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
-# 'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
-mkdir -p ~/.ssh
-eval $(ssh-agent -s)
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-ssh-add <(echo "$PRIVATE_SSH_KEY")
-
 rsync -r Dockerfile nginx.conf webroot $D_USER@$D_HOST:$D_LOCATION
 
 ssh $D_USER@$D_HOST "docker rm -f streaming-server && docker build -t streaming-server $D_LOCATION"
