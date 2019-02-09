bbaa2711f39a547b56c3fca496b4164e49bf2e62
Ind3xOnGitHub <me@ind3x.site>
2019-02-09 13:25:27 +0100

    Test Brotli compression

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+brotli --best --rm webroot/error.html
+brotli --best --rm webroot/index.html
+brotli --best --rm webroot/scripts.js
+brotli --best --rm webroot/styles.css
+
+ls -la webroot
+
 rsync -r Dockerfile nginx.conf webroot $D_USER@$D_HOST:$D_LOCATION
 
 ssh $D_USER@$D_HOST "docker rm -f streaming-server && docker build -t streaming-server $D_LOCATION"
