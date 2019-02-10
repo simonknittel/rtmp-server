a8a8e2ed880bed25ae0365e22e966a9854cb9f1d
Ind3xOnGitHub <me@ind3x.site>
2019-02-10 10:02:10 +0100

    Clean up

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+# Deploy
 rsync -r Dockerfile nginx.conf webroot $D_USER@$D_HOST:$D_LOCATION
 
+# Rebuild and restart Docker container
 ssh $D_USER@$D_HOST "docker rm -f streaming-server && docker build -t streaming-server $D_LOCATION"
 ssh $D_USER@$D_HOST "docker run -d -p 1935:1935 -p 80:80 --name streaming-server --restart always streaming-server"
